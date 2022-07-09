8bc072d132f80b71b1f9cd592448beaad3e238a2
Fernando05-blip <108412468+Fernando05-blip@users.noreply.github.com>
2022-07-08 21:19:26 -0400

    ejemplo javascript

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var miTitulo = document.getElementById('titulo');
+miTitulo.innerHTML = '¡Hola Mundo!';
+var miSubTitulo = document.getElementById('subtitulo');
+miSubTitulo.innerHTML = 'bienvenidos';
+var hora = document.getElementsByClassName('hora');
+hora.innerHTML = '19:33';
+
